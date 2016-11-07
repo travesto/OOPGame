3bfdaf0ec6974d8371da113e3b1ccbd95f41d484
travesto <tdc12c@acu.edu>
2016-11-07 14:34:23 -0600

    OOPGame

--- game.h
+++ game.h
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class game
 {
     public:
+        game() //ctor
+        ~game() //dtor
+        void input(char z) {};
+        string showMap(char m) {};
+        void currentLoc() {};
+        void deadlineDec(bool x) {};
+        void deadlineInc(bool y) {};
+        int checkTime(char t) {return deadline;}
     private:
-    
-}
+        int deadline = 500;
+};
+/*
+    Behaviours
+        - Receive input (Check t/f for valid input)
+            - void input(char x) { //pass to appropriate object};
+        - Map
+            - string showMap(char m) {return ascii map w/ currentLoc marked}
+        - Current Location
+            - void currentLoc() { //changes value in map depending on movement};    
+        - Decrement Deadline (If t, deadline--)
+            - void deadlineDec(bool x) { t--;}
+        - Increment Deadline
+            - void deadlineInc(bool x) {t++;}
+        - Check Time
+            - int checkTime(char t) {return deadline;}
+        - Variables
+            - int deadline = 500;
+*/
