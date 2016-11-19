0ca020a015d6946b05d529df053a492ec641bc3c
isaiah.moore <moorei1995@gmail.com>
2016-11-19 13:43:18 -0600

    NPC header anc cpp
    Everything, but the random dialogue works.

---
+++ NPC.h
@@ -0,0 +1,18 @@
+#ifndef NPC_h
+#define NPC_h
+#include <iostream>
+
+class NPC {
+	public:
+		NPC(std::string); //Give an NPC a name
+		~NPC() {}
+		std::string getDialogue();
+		void setDialogue();
+		
+		
+	private:
+		std::string name;
+		std::string npcDialogue;
+};
+
+#endif
